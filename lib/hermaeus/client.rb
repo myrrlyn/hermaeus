8665672b57cbaab6acefae570f6b3be2c262d896
myrrlyn <myrrlyn@outlook.com>
2016-10-11 14:53:33 -0400

    Enable the Client to scrape the global Compendium index for links

--- lib/hermaeus/client.rb
+++ lib/hermaeus/client.rb
@@ -1,5 +1,6 @@
 %w[
 	htmlentities
+	nokogiri
 	redd
 ].each(&method(:require))
 
@@ -19,6 +20,32 @@ module Hermaeus
 			@client = Redd.it(client.delete(:type).to_sym, *client.values, user_agent: USER_AGENT)
 			@client.authorize!
 			@html_filter = HTMLEntities.new
+		end
+
+		# Public: Scrapes the Compilation full index
+		#
+		# Optional parameters:
+		#
+		# query: A CSS selector string that will be used to target the HTML nodes
+		# desired.
+		#
+		# Returns a String Array containing the links pointed to by the Compendium
+		# index page.
+		def get_global_listing **opts
+			# This is a magic string that targets the index format /r/teslore uses to
+			# enumerate their Compendium.
+			query = opts[:query] || "td:first-child a"
+			# The returned HTML content is escaped, so it cannot be parsed as HTML.
+			fetch = @client.get("/r/teslore/wiki/compilation").body[:data]
+			# Rebind fetch to be unescaped HTML that Nokogiri can actually parse.
+			fetch = @html_filter.decode(fetch[:content_html])
+			# Feed the HTML text to Nokogiri for parsing.
+			doc = Nokogiri::HTML(fetch)
+			# CSS select all the links referenced by the index page. The CSS selector
+			# method returns Nokogiri objects, which are mapped down to the href data.
+			doc.css(query).map do |item|
+				item.attributes["href"].value
+			end
 		end
 	end
 end
